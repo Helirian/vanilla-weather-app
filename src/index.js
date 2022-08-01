75df930350a2b32069235c5eaeb6699f88aea2aa
Helirian <omoniieva@gmail.com>
2022-08-01 19:32:47 +0300

    updated time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+function formatDate(timestamp) {
+    // calculate the date
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[date.getDay()];
+
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-    console.log(response.data);
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.name;
     let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +34,8 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = response.data.wind.speed;
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "81c95f81174bafa543a7ffc89b06ec2a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=kyiv&appid=${apiKey}&units=metric`;

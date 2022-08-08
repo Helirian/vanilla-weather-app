7a50e1fe30c6b04c70fed245a8ef8214ee48eb71
Helirian <omoniieva@gmail.com>
2022-08-08 18:39:34 +0300

    added API to forecast

--- src/index.js
+++ src/index.js
@@ -23,32 +23,46 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-    days.forEach(function(day) {
-        forecastHTML =
-            forecastHTML +
-            `
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
+            forecastHTML =
+                forecastHTML +
+                `
                         <div class="col-2">
                             <div class="forecast-date">
-                                ${day}</div>
-                                <img src="" alt="" />
+                                ${formatDay(forecastDay.dt)}</div>
+                                <img src="http://openweathermap.org/img/wn/${
+                                  forecastDay.weather[0].icon
+                                }@2x.png" alt="" />
                             
                             <div class="forecast-temperatures">
-                                <span class="forecast-temperature-max">29</span>
-                                <span class="forecast-temperature-min">23</span>
+                                <span class="forecast-temperature-max">${Math.round(
+                                  forecastDay.temp.max
+                                )}°</span>
+                                <span class="forecast-temperature-min">${Math.round(
+                                  forecastDay.temp.min
+                                )}°</span>
                             </div>
                         </div>
                     `;
+        }
     });
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
-
-    //     forecastHTML = forecastHTML + ``;
 }
 
 function getForecast(coordinates) {
